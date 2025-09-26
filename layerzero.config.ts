38c315012c1e980a12e1e68decc39389c91c9811
Kaze <kaze@cow.fi>
2025-09-26 15:35:59 +0900

    update lz config to me more concrete

--- layerzero.config.ts
+++ layerzero.config.ts
@@ -20,29 +20,39 @@ import type { OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
  *         },
  *     },
  */
-const mainnetContract: OmniPointHardhat = {
-    eid: EndpointId.ETHEREUM_V2_MAINNET,
-    contractName: 'CowOftAdapter',
-}
 
-const bscContract: OmniPointHardhat = {
-    eid: EndpointId.BSC_V2_MAINNET,
-    contractName: 'CowOft',
-}
+// the list of networks to confirgure
+// hints on a good confirmation value to put are here:
+// https://docs.layerzero.network/v1/developers/evm/technical-reference/mainnet/default-config
+const networks: {
+    contract: OmniPointHardhat,
+    confirmations: number
+}[] = [
+    {
+        contract: {
+            eid: EndpointId.ETHEREUM_V2_MAINNET,
+            contractName: 'CowOftAdapter',
+        },
+        confirmations: 15
+    },
+    {
+        contract: {
+            eid: EndpointId.BSC_V2_MAINNET,
+            contractName: 'CowOft',
+        },
+        confirmations: 20,
+    },
+    {
+        contract: {
+            eid: EndpointId.AVALANCHE_V2_MAINNET,
+            contractName: 'CowOft',
+        },
+        confirmations: 12,
+    }
+];
 
-const avalancheContract: OmniPointHardhat = {
-    eid: EndpointId.AVALANCHE_V2_MAINNET,
-    contractName: 'CowOft',
-}
-
-// To connect all the above chains to each other, we need the following pathways:
-// Mainnet <-> BSC
-// Mainnet <-> Avalanche
-// BSC <-> Avalanche
-
-// For this example's simplicity, we will use the same enforced options values for sending to all chains
-// For production, you should ensure `gas` is set to the correct value through profiling the gas usage of calling OFT._lzReceive(...) on the destination chain
 // To learn more, read https://docs.layerzero.network/v2/concepts/applications/oapp-standard#execution-options-and-enforced-settings
+// We use the LZ reccomended 80000 gas for because we are using their ERC20 contract without any modifications to the receiving or transfer functions
 const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
     {
         msgType: 1,
@@ -54,35 +64,27 @@ const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
 
 // With the config generator, pathways declared are automatically bidirectional
 // i.e. if you declare A,B there's no need to declare B,A
-const pathways: TwoWayConfig[] = [
-    [
-        mainnetContract,
-        bscContract,
-        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
-        [1, 1], // [A to B confirmations, B to A confirmations]
-        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
-    ],
-    [
-        mainnetContract,
-        avalancheContract,
-        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
-        [1, 1], // [A to B confirmations, B to A confirmations]
-        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
-    ],
-    [
-        avalancheContract,
-        bscContract,
-        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
-        [1, 1], // [A to B confirmations, B to A confirmations]
-        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
-    ],
-]
+const pathways: TwoWayConfig[] = [];
+
+for (let i = 0;i < networks.length;i++) {
+    for (let j = i + 1;j < networks.length;j++) {
+        pathways.push([
+            networks[i].contract,
+            networks[j].contract,
+            [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+            [networks[i].confirmations, networks[j].confirmations], // [A to B confirmations, B to A confirmations]
+            [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
+        ])
+    }
+}
+
+console.log(`generated ${pathways.length} configuration pathways`);
 
 export default async function () {
     // Generate the connections config based on the pathways
     const connections = await generateConnectionsConfig(pathways)
     return {
-        contracts: [{ contract: mainnetContract }, { contract: bscContract }, { contract: avalancheContract }],
+        contracts: networks,
         connections,
     }
 }
