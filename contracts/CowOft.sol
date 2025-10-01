42c25eb6198aec0d7047338a51ae87f1de4c7a5d
Kaze <kaze@cow.fi>
2025-10-02 01:57:18 +0900

    switch to MIT or apache license

--- contracts/CowOft.sol
+++ contracts/CowOft.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.30;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
