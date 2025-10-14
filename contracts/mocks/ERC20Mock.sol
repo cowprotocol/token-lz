a47ebf0cf794d1b7a3196cfe6d21a6b65a998ade
Kaze <kaze@cow.fi>
2025-10-14 17:21:30 +0900

    feat: add additional tests
    these tests were pulled over from the generated repo from `npx create-lz-app --example oft` without selecting the OFTAdapter when prompted

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -1,8 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.20;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// @dev WARNING: This is for testing purposes only
 contract ERC20Mock is ERC20 {
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
 
--- test/foundry/CowOft.t.sol
+++ test/foundry/CowOft.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 // Mock imports
 import {CowOftMock} from "../../contracts/mocks/CowOftMock.sol";
-import {CowOftAdapterMock} from "../../contracts/mocks/CowOftAdapterMock.sol";
 import {ERC20Mock} from "../../contracts/mocks/ERC20Mock.sol";
 import {OFTComposerMock} from "../../contracts/mocks/OFTComposerMock.sol";
 
@@ -21,97 +20,113 @@ import {OFTComposeMsgCodec} from "@layerzerolabs/oft-evm/contracts/libs/OFTCompo
 
 // OZ imports
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-
-// Forge imports
-import "forge-std/console.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 // DevTools imports
 import {TestHelperOz5} from "@layerzerolabs/test-devtools-evm-foundry/contracts/TestHelperOz5.sol";
 
-contract CowOftAdapterTest is TestHelperOz5 {
+contract CowOftTest is TestHelperOz5, EIP712 {
     using OptionsBuilder for bytes;
 
     uint32 private aEid = 1;
     uint32 private bEid = 2;
 
-    ERC20Mock private aToken;
-    CowOftAdapterMock private aOFTAdapter;
+    CowOftMock private aOFT;
     CowOftMock private bOFT;
 
-    address private userA = address(0x1);
-    address private userB = address(0x2);
+    address private userA;
+    uint256 private userAKey;
+    address private userB;
+    uint256 private userBKey;
+
     uint256 private initialBalance = 100 ether;
 
+    constructor() EIP712("CoW Protocol Token", "1") {}
+
     function setUp() public virtual override {
+        (userA, userAKey) = makeAddrAndKey("userA");
+        (userB, userBKey) = makeAddrAndKey("userB");
+
         vm.deal(userA, 1000 ether);
         vm.deal(userB, 1000 ether);
 
         super.setUp();
         setUpEndpoints(2, LibraryType.UltraLightNode);
 
-        aToken = ERC20Mock(_deployOApp(type(ERC20Mock).creationCode, abi.encode("Token", "TOKEN")));
+        vm.startPrank(address(1234));
+        aOFT =
+            CowOftMock(_deployOApp(type(CowOftMock).creationCode, abi.encode(address(endpoints[aEid]), address(this))));
 
-        aOFTAdapter = CowOftAdapterMock(
-            _deployOApp(
-                type(CowOftAdapterMock).creationCode, abi.encode(address(aToken), address(endpoints[aEid]), address(this))
-            )
-        );
-
-        bOFT = CowOftMock(
-            _deployOApp(
-                type(CowOftMock).creationCode, abi.encode(address(endpoints[bEid]), address(this))
-            )
-        );
+        bOFT =
+            CowOftMock(_deployOApp(type(CowOftMock).creationCode, abi.encode(address(endpoints[bEid]), address(this))));
+        vm.stopPrank();
 
         // config and wire the ofts
         address[] memory ofts = new address[](2);
-        ofts[0] = address(aOFTAdapter);
+        ofts[0] = address(aOFT);
         ofts[1] = address(bOFT);
         this.wireOApps(ofts);
 
         // mint tokens
-        aToken.mint(userA, initialBalance);
+        aOFT.mint(userA, initialBalance);
+        bOFT.mint(userB, initialBalance);
     }
 
     function test_constructor() public view {
-        assertEq(aOFTAdapter.owner(), address(this));
+        assertEq(aOFT.owner(), address(this));
         assertEq(bOFT.owner(), address(this));
 
-        assertEq(aToken.balanceOf(userA), initialBalance);
-        assertEq(aToken.balanceOf(address(aOFTAdapter)), 0);
-        assertEq(bOFT.balanceOf(userB), 0);
+        assertEq(aOFT.balanceOf(userA), initialBalance);
+        assertEq(bOFT.balanceOf(userB), initialBalance);
 
-        assertEq(aOFTAdapter.token(), address(aToken));
+        assertEq(aOFT.token(), address(aOFT));
         assertEq(bOFT.token(), address(bOFT));
 
+        assertEq(aOFT.name(), "CoW Protocol Token");
         assertEq(bOFT.name(), "CoW Protocol Token");
+
+        assertEq(aOFT.symbol(), "COW");
         assertEq(bOFT.symbol(), "COW");
+
+        assertEq(address(aOFT.endpoint()), endpoints[aEid]);
+        assertEq(address(bOFT.endpoint()), endpoints[bEid]);
+
+        (
+            ,
+            string memory name,
+            string memory version,
+            uint256 chainId,
+            address verifyingContract,
+            bytes32 salt,
+            uint256[] memory extensions
+        ) = aOFT.eip712Domain();
+        assertEq(name, "CoW Protocol Token");
+        assertEq(version, "1");
+        assertEq(chainId, 31337);
+        assertEq(verifyingContract, address(aOFT));
+        assertEq(salt, bytes32(0));
+        assertEq(extensions, new uint256[](0));
     }
 
-    function test_send_oft_adapter() public {
+    function test_send_oft() public {
         uint256 tokensToSend = 1 ether;
         bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(200000, 0);
         SendParam memory sendParam =
             SendParam(bEid, addressToBytes32(userB), tokensToSend, tokensToSend, options, "", "");
-        MessagingFee memory fee = aOFTAdapter.quoteSend(sendParam, false);
+        MessagingFee memory fee = aOFT.quoteSend(sendParam, false);
 
-        assertEq(aToken.balanceOf(userA), initialBalance);
-        assertEq(aToken.balanceOf(address(aOFTAdapter)), 0);
-        assertEq(bOFT.balanceOf(userB), 0);
-
-        vm.prank(userA);
-        aToken.approve(address(aOFTAdapter), tokensToSend);
+        assertEq(aOFT.balanceOf(userA), initialBalance);
+        assertEq(bOFT.balanceOf(userB), initialBalance);
 
         vm.prank(userA);
-        aOFTAdapter.send{value: fee.nativeFee}(sendParam, fee, payable(address(this)));
+        aOFT.send{value: fee.nativeFee}(sendParam, fee, payable(address(this)));
         verifyPackets(bEid, addressToBytes32(address(bOFT)));
 
-        assertEq(aToken.balanceOf(userA), initialBalance - tokensToSend);
-        assertEq(aToken.balanceOf(address(aOFTAdapter)), tokensToSend);
-        assertEq(bOFT.balanceOf(userB), tokensToSend);
+        assertEq(aOFT.balanceOf(userA), initialBalance - tokensToSend);
+        assertEq(bOFT.balanceOf(userB), initialBalance + tokensToSend);
     }
 
-    function test_send_oft_adapter_compose_msg() public {
+    function test_send_oft_compose_msg() public {
         uint256 tokensToSend = 1 ether;
 
         OFTComposerMock composer = new OFTComposerMock();
@@ -121,18 +136,14 @@ contract CowOftAdapterTest is TestHelperOz5 {
         bytes memory composeMsg = hex"1234";
         SendParam memory sendParam =
             SendParam(bEid, addressToBytes32(address(composer)), tokensToSend, tokensToSend, options, composeMsg, "");
-        MessagingFee memory fee = aOFTAdapter.quoteSend(sendParam, false);
+        MessagingFee memory fee = aOFT.quoteSend(sendParam, false);
 
-        assertEq(aToken.balanceOf(userA), initialBalance);
-        assertEq(aToken.balanceOf(address(aOFTAdapter)), 0);
-        assertEq(bOFT.balanceOf(userB), 0);
-
-        vm.prank(userA);
-        aToken.approve(address(aOFTAdapter), tokensToSend);
+        assertEq(aOFT.balanceOf(userA), initialBalance);
+        assertEq(bOFT.balanceOf(address(composer)), 0);
 
         vm.prank(userA);
         (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt) =
-            aOFTAdapter.send{value: fee.nativeFee}(sendParam, fee, payable(address(this)));
+            aOFT.send{value: fee.nativeFee}(sendParam, fee, payable(address(this)));
         verifyPackets(bEid, addressToBytes32(address(bOFT)));
 
         // lzCompose params
@@ -146,8 +157,7 @@ contract CowOftAdapterTest is TestHelperOz5 {
         );
         this.lzCompose(dstEid_, from_, options_, guid_, to_, composerMsg_);
 
-        assertEq(aToken.balanceOf(userA), initialBalance - tokensToSend);
-        assertEq(aToken.balanceOf(address(aOFTAdapter)), tokensToSend);
+        assertEq(aOFT.balanceOf(userA), initialBalance - tokensToSend);
         assertEq(bOFT.balanceOf(address(composer)), tokensToSend);
 
         assertEq(composer.from(), from_);
@@ -157,5 +167,28 @@ contract CowOftAdapterTest is TestHelperOz5 {
         assertEq(composer.extraData(), composerMsg_); // default to setting the extraData to the message as well to test
     }
 
-    // TODO import the rest of oft tests?
+    function test_permit() external {
+        uint256 permitAmount = 1 ether;
+        assertEq(aOFT.allowance(userA, userB), 0);
+
+        bytes32 structHash = keccak256(
+            abi.encode(
+                keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"),
+                userA,
+                userB,
+                permitAmount,
+                aOFT.nonces(userA),
+                type(uint256).max
+            )
+        );
+
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", aOFT.DOMAIN_SEPARATOR(), structHash));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userAKey, digest);
+
+        aOFT.permit(userA, userB, permitAmount, type(uint256).max, v, r, s);
+
+        // verify that the allowance was set on the token contract
+        assertEq(aOFT.allowance(userA, userB), permitAmount);
+    }
 }
